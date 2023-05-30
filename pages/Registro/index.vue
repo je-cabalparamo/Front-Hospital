23203a5eaf5106d6256f6fe89e46eedf66436958
Bryan Ricardo Cervantes Mancera <53015568+BryanRicardo@users.noreply.github.com>
2023-05-30 16:20:09 -0600

    Inicio del Front Hospital

---
+++ pages/Registro/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <tablaUsuarios />
+</template>
+
+<script>
+import tablaUsuarios from '~/components/registroC/registroC.vue'
+export default {
+  name: 'IndexPage',
+  auth: true,
+  components: {
+    tablaUsuarios
+  },
+  layout: 'main'
+}
+</script>
